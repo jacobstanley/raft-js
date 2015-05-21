176ce4854f7acacf2298b5751ad5d4c5003c22c4
Jacob Stanley <jacob@stanley.io>
2015-05-21 17:11:57 +1000

    Playing with ghcjs-dom

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,18 @@ module Main (main) where
 
 import           Control.Concurrent (threadDelay)
 import           Control.Monad (forever)
+import           Control.Monad.Trans (liftIO)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Text as T
 import qualified JavaScript.WebSockets as WS
+
+import qualified GHCJS.DOM as X
+import qualified GHCJS.DOM.Document as X
+import qualified GHCJS.DOM.Element as X
+import qualified GHCJS.DOM.EventM as X
+import qualified GHCJS.DOM.HTMLElement as X
+import qualified GHCJS.DOM.HTMLParagraphElement as X
+import qualified GHCJS.DOM.Node as X
 
 import           ClientTypes
 
@@ -19,16 +28,31 @@ receiveResponse :: WS.Connection -> IO (ClientResponse B.ByteString Int)
 receiveResponse conn = fromText <$> WS.receiveText conn
 
 main :: IO ()
-main = WS.withUrl "ws://localhost:3000" $ \conn ->
-  forever $ do
-    sendRequest conn (CmdSet "x" 7)
-    receiveResponse conn >>= print
-    sendRequest conn (CmdSet "y" 8)
-    receiveResponse conn >>= print
-    sendRequest conn (CmdSet "x" 9)
-    receiveResponse conn >>= print
-    sendRequest conn (CmdSet "x" 2)
-    receiveResponse conn >>= print
-    sendRequest conn (CmdGet "x")
-    receiveResponse conn >>= print
-    threadDelay 1000000
+main = run $ \conn webView -> do
+    Just doc <- X.webViewGetDomDocument webView
+    Just body <- X.documentGetBody doc
+    X.htmlElementSetInnerHTML body ("<h1>Hello World</h1>" :: String)
+    X.elementOnclick body $ do
+      (x, y) <- X.mouseClientXY
+      liftIO $ do
+        X.htmlElementSetInnerHTML body ("<h1>Responses:</h1>" :: String)
+        reqRsp conn doc body (CmdSet "x" x)
+        reqRsp conn doc body (CmdSet "y" y)
+        return ()
+    forever (threadDelay 1000000 >> return ())
+  where
+    run io = X.runWebGUI $ \view ->
+             WS.withUrl "ws://localhost:3000" $ \conn ->
+             io conn view
+
+    reqRsp conn doc body cmd = do
+      addText doc body (show cmd)
+      sendRequest conn cmd
+      rsp <- receiveResponse conn
+      addText doc body (show rsp)
+
+    addText doc body str = do
+      Just newParagraph <- fmap X.castToHTMLParagraphElement
+                           <$> X.documentCreateElement doc ("p" :: String)
+      X.htmlElementSetInnerText newParagraph str
+      X.nodeAppendChild body (Just newParagraph)
